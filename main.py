0d0d333f93cfeb9ae718b2a1d80b919e36045337
Calc4me <126410001+Calc4me@users.noreply.github.com>
2024-01-12 08:48:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import random
+from random import randint as r
+#Encrypt function
 def scramble(n):
+     #Space
      if(n == 32):
-          return 166 + random.randint(1, 10)
+          #It's harder to tell what are spaces if it's slightly random.
+          return 166 + r(1, 10)
+     #Unscramble space
      if(167 <= n <= 175):
+          #Space
           return 32
+     #Not space
      else:
           return 156 - n
 message = input()
 values = []
+#Put into values
 for i in range(len(message)):
      values.append(ord(message[i]))
+#Scramble and print
 for j in range(len(values)):
      print(chr(scramble(values[j])), end="")
